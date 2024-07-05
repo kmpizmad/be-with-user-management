2c7ddae038ea75f7f92135717f7324359be49aa5
kmpizmad <nagy.viktor392@gmail.com>
2024-07-05 22:12:13 +0200

    fix: update db schema

--- src/lib/schemas/user.ts
+++ src/lib/schemas/user.ts
@@ -11,7 +11,7 @@ const passwordSchema = z.string({
 
 export const userRegisterSchema = z.object({
   email: emailSchema,
-  password: passwordSchema.min(8),
+  password: passwordSchema.min(8, { message: "'password' must contain at least 8 characters" }),
   firstName: z.string({
     required_error: "'firstName' is required",
     invalid_type_error: "'firstName' must be type of string",
@@ -21,9 +21,11 @@ export const userRegisterSchema = z.object({
     invalid_type_error: "'lastName' must be type of string",
   }),
   active: z.boolean({ invalid_type_error: "'active' must be type of boolean" }).optional(),
-  roles: z.array(z.string({ invalid_type_error: "Items of 'roles' must be type of string" }), {
-    required_error: "'roles' is required",
-  }),
+  roles: z
+    .array(z.string({ invalid_type_error: "Items of 'roles' must be type of string" }), {
+      required_error: "'roles' is required",
+    })
+    .min(1, { message: "'roles' must contain at least 1 item" }),
 });
 
 export type UserRegisterSchema = z.infer<typeof userRegisterSchema>;
